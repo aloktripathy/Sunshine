77dcf201a3b8587b7cb580c3b2970dbd33a7e5bd
Alok Tripathy <alok@healthifyme.com>
2016-08-18 02:25:28 +0530

    Display Content in DetailActivity

---
+++ app/src/main/java/com/aloktripathy/sunshine/DetailActivity.java
@@ -0,0 +1,46 @@
+package com.aloktripathy.sunshine;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+public class DetailActivity extends AppCompatActivity implements DetailFragment.OnFragmentInteractionListener{
+    public static final String DETAIL_TEXT = "DETAIL_TEXT";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        DetailFragment detailFragment = new DetailFragment();
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container_detail, detailFragment)
+                .commit();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.detail, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.detail_settings:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+}
--- app/src/main/java/com/aloktripathy/sunshine/MainActivity.java
+++ app/src/main/java/com/aloktripathy/sunshine/MainActivity.java
@@ -1,13 +1,64 @@
 package com.aloktripathy.sunshine;
 
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+        implements ForecastFragment.OnFragmentInteractionListener{
+    private final boolean DEBUG = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.container, new ForecastFragment()).commit();
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return true;
+    }
+
+    public void showDetailActivity(String text) {
+        Intent intent = new Intent(this, DetailActivity.class);
+        intent.putExtra(DetailActivity.DETAIL_TEXT, text);
+        startActivity(intent);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return false;
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        /*
+         * The system calls onSaveInstanceState() before making the activity vulnerable
+         * to destruction. The system passes this method a Bundle in which you can save
+         * state information about the activity as name-value pairs, using methods such
+         * as putString() and putInt()
+         */
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+    public void showToast(String message) {
+        Context context = getApplicationContext();
+        Toast toast = Toast.makeText(context, message, Toast.LENGTH_SHORT);
+        toast.show();
     }
 }
